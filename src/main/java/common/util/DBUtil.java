90bf54b776c6f8f9f4e8bc5ef4d31e6a00f2b1fc
YeLim <min_yl8234@naver.com>
2023-04-22 15:30:35 +0900

    커밋 5 기타 파일들 커밋

---
+++ src/main/java/common/util/DBUtil.java
@@ -0,0 +1,25 @@
+package common.util;
+import java.sql.*;
+
+public class DBUtil {
+	static String url = "jdbc:oracle:thin:@localhost:1521:XE";
+	static String user = "multishop", pwd = "tiger";
+	
+	static {
+		// 드라이버 로딩
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			System.out.println("Driver Loading Success!");
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	}	// main보다 먼저 실행
+	
+	public static Connection getCon() throws SQLException {
+		Connection con = DriverManager.getConnection(url, user, pwd);
+		return con;
+	}
+
+}
